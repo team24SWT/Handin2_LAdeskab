4e8c744419759e5b3c4f42cb7b602b78b500428b
Murat Eroglu <au638962@uni.au.dk>
2021-03-18 14:04:20 +0100

    Added implementation for the functions in Door Class

--- Handin2_LAdeskab/Classes/Door.cs
+++ Handin2_LAdeskab/Classes/Door.cs
@@ -19,19 +19,55 @@ namespace Handin2_LAdeskab
 
         public Door()
         {
-            DoorState = false;
-            LockState = true;
+            DoorState = false;  //Closed
+            LockState = true;   //Unlocked
         
         }
 
+        //Lock the door
         public void LockDoor()
-        { }
+        {
+            if (DoorState || !LockState)
+            {
+                return; // Returns if the door is open or locked
+            }
+            LockState = false;
+        }
+        //Unlock the door
         public void UnlockDoor()
-        { }
+        {
+            if (DoorState || LockState)
+            {
+                return; // Returns if the door is open or unlocked
+            }
+            LockState = true;
+        }
+        //Open the door
         public void DoorOpen()
-        { }
+        {
+            if (DoorState || !LockState)
+            {
+                return; // Returns if the door is open or locked
+            }
+            //Sets new states
+            doorEventState.DoorState = true;
+            DoorState = true;
+            //Event invoke
+            DoorEvent?.Invoke(this, doorEventState);
+        }
+        //CLose the door
         public void DoorClosed()
-        { }
+        {
+            if (!DoorState || !LockState)
+            {
+                return; // Returns if the door is closed or locked
+            }
+            //Sets new states
+            doorEventState.DoorState = false;
+            DoorState = false;
+            //Event invoke
+            DoorEvent?.Invoke(this, doorEventState);
+        }
 
     }//class
 }//namespace
