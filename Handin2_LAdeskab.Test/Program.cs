bb0854c7720c176f6070f59ba7b98d89803bea0b
Oleksandr Zubrytskyy <201908679@post.au.dk>
2021-03-25 18:14:53 +0100

    Main udkomenteret

--- Handin2_LAdeskab.Test/Program.cs
+++ Handin2_LAdeskab.Test/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Handin2_LAdeskab.Test
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace Handin2_LAdeskab.Test
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
